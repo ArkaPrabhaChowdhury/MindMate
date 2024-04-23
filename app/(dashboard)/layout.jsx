9869f35baf29da2f5c2fd5ae0cca565ceff460c7
Anirudh0807 <anirudhvarma9849@gmail.com>
2024-04-23 06:15:42 +0530

    Refactor layout and sidebar components

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -5,16 +5,20 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <body className="">
-        <main className="flex">
-          <LeftSidebar />
-
-          <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
-            <div className="w-full">
-              <CheckToken />
-              {children}
+        <div className="min-h-screen">
+          <main className="flex">
+            <div className="">
+              <LeftSidebar />
             </div>
-          </section>
-        </main>
+
+            <section className="bg-gray-900 flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
+              <div className="w-full">
+                <CheckToken />
+                {children}
+              </div>
+            </section>
+          </main>
+        </div>
       </body>
     </html>
   );
